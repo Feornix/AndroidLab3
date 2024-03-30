b35512dc40b74437a46b58281c0c98a88e72e66f
griml <grimlock.pheornix@bk.ru>
2024-03-30 21:15:24 +0600

    Создание фрагмента для ConstraintLayout

--- app/src/main/java/com/example/androidlab3/ConstraintLayoutFragment.java
+++ app/src/main/java/com/example/androidlab3/ConstraintLayoutFragment.java
@@ -7,14 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.androidlab3.databinding.FragmentConstraintLayoutBinding;
 import com.example.androidlab3.databinding.FragmentLinearLayoutBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LinearLayoutFragment#newInstance} factory method to
+ * Use the {@link ConstraintLayoutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LinearLayoutFragment extends Fragment {
+public class ConstraintLayoutFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +27,7 @@ public class LinearLayoutFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LinearLayoutFragment() {
+    public ConstraintLayoutFragment() {
         // Required empty public constructor
     }
 
@@ -35,11 +37,11 @@ public class LinearLayoutFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LinearLayoutFragment.
+     * @return A new instance of fragment ConstraintLayoutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LinearLayoutFragment newInstance(String param1, String param2) {
-        LinearLayoutFragment fragment = new LinearLayoutFragment();
+    public static ConstraintLayoutFragment newInstance(String param1, String param2) {
+        ConstraintLayoutFragment fragment = new ConstraintLayoutFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -47,7 +49,7 @@ public class LinearLayoutFragment extends Fragment {
         return fragment;
     }
 
-    FragmentLinearLayoutBinding binding;
+    FragmentConstraintLayoutBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +62,7 @@ public class LinearLayoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentLinearLayoutBinding.inflate(inflater, container, false);
+        binding = FragmentConstraintLayoutBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         // Inflate the layout for this fragment
         return view;
--- app/src/main/java/com/example/androidlab3/MainActivity.java
+++ app/src/main/java/com/example/androidlab3/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.androidlab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidlab3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ConstraintLayoutFragment fragment = new ConstraintLayoutFragment();
+        fragmentTransaction.replace(R.id.placeholder, fragment);
+        fragmentTransaction.commit();
+        setContentView(view);
     }
 }
